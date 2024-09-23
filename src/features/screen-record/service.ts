aaf8a801b5dbf34cbd3462148cc365c82f3f9ca9
tusharkantimaji <tusharkanti2001maji@gmail.com>
2024-09-23 14:53:47 +0530

    Changes

--- src/features/screen-record/service.ts
+++ src/features/screen-record/service.ts
@@ -3,11 +3,11 @@ import { getStream, launch } from 'puppeteer-stream';
 import * as fs from 'fs-extra';
 import { v4 as uuidv4 } from 'uuid';
 import { ScreenRecordReqDto } from './dto';
+import * as ffmpeg from 'fluent-ffmpeg';
+import * as path from 'path';
 
 @Injectable()
 export class ScreenRecordService {
-  private sessionFiles: string[] = [];
-
   public async startRecording(body: ScreenRecordReqDto): Promise<void> {
     const browser = await launch({
       executablePath:
@@ -25,16 +25,54 @@ export class ScreenRecordService {
     const fileStream = fs.createWriteStream(fileName);
     stream.pipe(fileStream);
 
-    this.sessionFiles.push(fileName);
-
     return new Promise<void>(async (resolve) => {
       setTimeout(async () => {
         stream.destroy();
         console.log('Stopping the recording...');
         fileStream.close();
         await browser.close();
+        await this.mergeVideos();
         resolve();
       }, body.time);
+    });
+  }
+
+  private async mergeVideos(): Promise<void> {
+    const inputFolder = './videos/';
+    const outputFolder = './video_output/';
+
+    const outputFile = 'final_video.mp4';
+
+    const files = fs
+      .readdirSync(inputFolder)
+      .filter((file) => file.endsWith('.webm'));
+
+    if (files.length === 0) {
+      throw new Error('No .webm files found to merge.');
+    }
+
+    let mergedVideo = ffmpeg();
+
+    files.forEach((file) => {
+      mergedVideo = mergedVideo.input(path.join(inputFolder, file));
+    });
+
+    const outputFilePath = path.join(outputFolder, outputFile);
+
+    return new Promise((resolve, reject) => {
+      mergedVideo
+        .on('start', (commandLine) => {
+          console.log('Started merging process with command: ', commandLine);
+        })
+        .on('error', (err) => {
+          console.error('Error occurred during merging:', err.message);
+          reject(`Error during merging: ${err.message}`);
+        })
+        .on('end', () => {
+          console.log('Merging finished successfully!');
+          resolve();
+        })
+        .mergeToFile(outputFilePath, outputFolder);
     });
   }
 }
